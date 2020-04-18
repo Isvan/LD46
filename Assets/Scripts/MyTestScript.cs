5da4f6e6e3df8f42d3f4048caf0ff92adfe37b1d
Iavor Ivanov <iavor@telus.net>
2020-04-17 21:29:15 -0700

    Added placeholder stuff

---
+++ Assets/Scripts/MyTestScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyTestScript : MonoBehaviour
+{
+
+    public float speed;
+    // Start is called before the first frame update
+    private Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+
+    private void FixedUpdate()
+    {
+
+
+    }
+}
