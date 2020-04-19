73a4cc5307edccea6dcbb74ffa6b4be1d927e613
Jennifer Gu <jen.jennifer.gu@gmail.com>
2020-04-19 14:46:46 -0700

    trap prefab tag

--- Assets/Scripts/CollisionDespawn.cs
+++ Assets/Scripts/CollisionDespawn.cs
@@ -6,16 +6,22 @@ public class CollisionDespawn : MonoBehaviour
 {
     public string Tag;
     public float interval;
-    public bool destroyTagObject;
+    public bool destroyTagObject = true;
+    public bool destroySelf = false;
     private void OnCollisionEnter(Collision collision)
     {
-  
-        if (collision.gameObject.CompareTag(Tag) && destroyTagObject == false)
+
+        if (collision.gameObject.CompareTag(Tag))
         {
-            Destroy(gameObject, interval);
-        }else if(destroyTagObject == true)
-        {
-            Destroy(collision.gameObject, interval);
+            if (destroyTagObject == true)
+            {
+                Destroy(collision.gameObject, interval);
+            }
+
+            if (destroySelf == true)
+            {
+                Destroy(gameObject, interval);
+            }
         }
     }
 }
